76c0f1fdcbc1b8d4a82a07ecffa781d5283eabd5
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-03-21 17:21:40 +0200

    add some migrations

---
+++ migrations/1584803839926-add-player-to-user.js
@@ -0,0 +1,28 @@
+const  {user:userDocument,artist:artistDocument,album:albumDocument,track:trackDocument,playlist:playlistDocument,category:categoryDocument} = require('../models/DB');
+const mongoose=require('mongoose');
+mongoose.Promise=global.Promise;
+// connect to db
+const atlas ='mongodb+srv://nada:nada@spotifycluster-i2m7n.mongodb.net/Spotify?retryWrites=true&w=majority';
+const localhost = 'mongodb://localhost:27017/test' ;
+const bahaa ="mongodb+srv://bahaaEldeen:123@spotifycluster-i2m7n.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(localhost,{  useNewUrlParser: true, useCreateIndex: true ,useUnifiedTopology:true});
+mongoose.connection.once('open',()=>{
+console.log("connection is made");
+}).on('error',function(error){
+console.log("connection got error : ",error);
+});
+// update all tracks to include artist id
+module.exports.up = async function (next) {
+  userDocument.find({},async (err,users)=>{
+    if(err) next(err);
+    for(let user of users){
+      user.player = {};
+      await user.save();
+    }
+  });
+  next()
+}
+
+module.exports.down = function (next) {
+  next()
+}
