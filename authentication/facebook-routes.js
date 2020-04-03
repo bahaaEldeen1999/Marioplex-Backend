5776a330d5bfdcf09742731185960f45b53eec19
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-04-03 19:08:40 +0200

    fchanged isMe to be like docs and facebbok route return token now

--- authentication/facebook-routes.js
+++ authentication/facebook-routes.js
@@ -41,11 +41,11 @@ router.get('/facebookJWT',checkAuthentication,(req,res)=>{
     const id = req.session.passport.user;
     //res.send('hh')
    var token = jwt.sign({ _id: id,product:req.session.passport.user.product}, jwtSeret.secret, {
-        expiresIn: '24h' // 1 day
+        expiresIn: '3209832702h' // 1 day
      });
      
       // return the information including token as JSON
-      res.json({success: true, token: 'JWT ' + token});
+      res.json({ token: token});
      // res.send(token);
 });
 
--- middlewares/isMe.js
+++ middlewares/isMe.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 const jwtSecret = require('../config/jwt-key').secret;
 function auth(req,res,next){
     
-const token=req.header('x-auth-token');
+const token=req.header('Authorization');
 
 if(!token){return res.status(401).send('No Available token');}
 
