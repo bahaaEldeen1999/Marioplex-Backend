c48114a2a52748ad03ef27241db5c88faf0c2e03
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-04-03 19:10:42 +0200

    Update isMe.js

--- middlewares/isMe.js
+++ middlewares/isMe.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken');
 const jwtSecret = require('../config/jwt-key').secret;
 function auth(req,res,next){
     
-const token=req.header('Authorization');
+const token=req.header('x-auth-token');
 
 if(!token){return res.status(401).send('No Available token');}
 
