2b3d63ad5ab123ddbe319d535dee0b0c41fc4559
habohadima <hsabohadima@gmail.com>
2020-04-08 16:57:11 +0200

    send-email

--- forget-password/sendmail.js
+++ forget-password/sendmail.js
@@ -1,26 +1,32 @@
 var nodemailer = require('nodemailer');
-module.exports=function(email,password){
-var transporter = nodemailer.createTransport({
-  host:'smtp.gmail.com',
-  port:587,
-  secure:false,
-  auth: {
-    user: 'appspotify646@gmail.com',
-    pass: 'Helloworld55'
-  },
-  tls:{
-    rejectUnauthorized:false
-  }
-});
+/**
+ * send email
+ * @param {string} email - user email
+ * @param {string} password -email password
+ */
+function sendEmail(email, password) {
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: 'appspotify646@gmail.com',
+            pass: 'Helloworld55'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-var mailOptions = {
-  from: '"Spotify Contact" <appspotify646@gmail.com>',
-  to: email,
-  subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
-  text: password
-};
+    var mailOptions = {
+        from: '"Spotify Contact" <appspotify646@gmail.com>',
+        to: email,
+        subject: 'SPOTIFY SAMA has A Message FOR YOU ^^',
+        text: password
+    };
 
-transporter.sendMail(mailOptions, function(error, info){
- 
-});
+    transporter.sendMail(mailOptions, function(error, info) {
+
+    });
 };
+module.exports = sendEmail;
