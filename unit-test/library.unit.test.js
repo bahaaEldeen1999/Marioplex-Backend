ce6ecab2c61b1e023608e487c2da45692b4f8e5c
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-04-11 16:51:13 +0200

    library 100

--- unit-test/library.unit.test.js
+++ unit-test/library.unit.test.js
@@ -5,7 +5,62 @@ const user = {
     saveAlbum: [{ albumId: "album1" }, { albumId: "album2" }, { albumId: "album3" }, { albumId: "album4" }, { albumId: "album5" }, { albumId: "album6" }, { albumId: "album7" }],
     like: [{ trackId: "track1" }, { trackId: "track2" }, { trackId: "track3" }, { trackId: "track4" }, { trackId: "track5" }, { trackId: "track6" }, { trackId: "track7" }, { trackId: "track8" }]
 }
-
+const user2 = {
+    id:"2",
+    saveAlbum:[],
+    like:[]
+}
+const user3 = {
+    id:"3",
+    saveAlbum:[
+        { albumId: "1" },
+        { albumId: "11" },
+        { albumId: "111" },
+        { albumId: "12" },
+        { albumId: "13" },
+        { albumId: "14" },
+        { albumId: "15" },
+        { albumId: "16" },
+        { albumId: "176" },
+        { albumId: "17" },
+        { albumId: "18" },
+        { albumId: "19" },
+        { albumId: "10" },
+        { albumId: "1111" },
+        { albumId: "21" },
+        { albumId: "31" },
+        { albumId: "41" },
+        { albumId: "51" },
+        { albumId: "61" },
+        { albumId: "81" },
+        { albumId: "71" },
+        { albumId: "91" },
+    ],
+    like:[
+        { trackId: "1" },
+        { trackId: "11" },
+        { trackId: "111" },
+        { trackId: "12" },
+        { trackId: "13" },
+        { trackId: "14" },
+        { trackId: "15" },
+        { trackId: "16" },
+        { trackId: "176" },
+        { trackId: "17" },
+        { trackId: "18" },
+        { trackId: "19" },
+        { trackId: "10" },
+        { trackId: "1111" },
+        { trackId: "21" },
+        { trackId: "31" },
+        { trackId: "41" },
+        { trackId: "51" },
+        { trackId: "61" },
+        { trackId: "81" },
+        { trackId: "71" },
+        { trackId: "91" },
+    ]
+}
 test('check saved albums', () => {
 
 
@@ -31,4 +86,61 @@ test('get saved tracks', () => {
 
     // expect that user has saved tracks 
     expect(mockLibrary.getSavedTracks(user, 2)).toEqual(["track1", "track2"]);
+})
+
+test("get saved album of non user",()=>{
+    expect(mockLibrary.getSavedAlbums(undefined)).toBeFalsy();
+})
+
+test("get saved album of user with no saved albums",()=>{
+    expect(mockLibrary.getSavedAlbums(user2)).toBeFalsy();
+})
+
+test("get saved album of user with more than 20 alums",()=>{
+    expect(mockLibrary.getSavedAlbums(user3)).toBeTruthy();
+})
+
+test("get saved album of user with more than 20 alums with offset",()=>{
+    expect(mockLibrary.getSavedAlbums(user3,20,10)).toBeTruthy();
+})
+
+test("get saved album of user with more than 20 alums with wromg offset",()=>{
+    expect(mockLibrary.getSavedAlbums(user3,20,10000)).toBeTruthy();
+})
+
+test("get saved album of user with more than 20 alums with limit",()=>{
+    expect(mockLibrary.getSavedAlbums(user3,5,10000)).toBeTruthy();
+})
+
+test("get saved album of user with more than 20 alums with wrong  limit",()=>{
+    expect(mockLibrary.getSavedAlbums(user3,5,10000)).toBeTruthy();
+})
+
+
+test("get saved track of non user",()=>{
+    expect(mockLibrary.getSavedTracks(undefined)).toBeFalsy();
+})
+
+test("get saved track of user with no saved track",()=>{
+    expect(mockLibrary.getSavedTracks(user2)).toBeFalsy();
+})
+
+test("get saved track of user with more than 20 track",()=>{
+    expect(mockLibrary.getSavedTracks(user3)).toBeTruthy();
+})
+
+test("get saved track of user with more than 20 track with offset",()=>{
+    expect(mockLibrary.getSavedTracks(user3,20,10)).toBeTruthy();
+})
+
+test("get saved track of user with more than 20 track with wrong offset",()=>{
+    expect(mockLibrary.getSavedTracks(user3,20,10000)).toBeTruthy();
+})
+
+test("get saved track of user with more than 20 track with limit",()=>{
+    expect(mockLibrary.getSavedTracks(user3,5,10000)).toBeTruthy();
+})
+
+test("get saved track of user with more than 20 track with wrong  limit",()=>{
+    expect(mockLibrary.getSavedTracks(user3,5,10000)).toBeTruthy();
 })
