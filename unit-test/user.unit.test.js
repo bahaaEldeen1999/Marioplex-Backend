326065f61ef6521be3ce89d9b757dd407b31d32d
bahaaEldeen <30871441+bahaaEldeen1999@users.noreply.github.com>
2020-04-11 16:37:36 +0200

    user test 100

--- unit-test/user.unit.test.js
+++ unit-test/user.unit.test.js
@@ -29,7 +29,22 @@ UserTest.user = [{
         'userType': 'user',
         'isFacebook': true,
         'follow': [{ id: '2' }]
-    }
+    },
+    {
+        '_id':'4',
+        'follow': [],
+        'createPlaylist':[]
+    },
+    {
+        '_id':'5',
+        'follow': [{id:'4'},{id:'1'},{id:'44'},{id:'5'},{id:'12'}],
+        
+    },
+    {
+        '_id':'6',
+        'follow': [],
+        'createPlaylist':['1','2','3']
+    },
 ]
 UserTest.Artists = [{
         'id': '12',
@@ -114,8 +129,91 @@ test(('promote user to artist'), () => {
 test(('promote user to artist'), () => {;
         expect(UserTest.promoteToArtist('3')).toEqual(false)
     })
-    // delete account at end 
+
+
+test('updaet facebook user with country',()=>{
+    expect(UserTest.update("2",undefined,undefined,undefined,"eg")).toBeTruthy();
+})
+
+
+test('update facebook user without country',()=>{
+    expect(UserTest.update("2",undefined,undefined,undefined,undefined)).toBeTruthy();
+})
+
+test('update non existing user',()=>{
+    expect(UserTest.update("99",undefined,undefined,undefined,undefined)).toBeFalsy();
+})
+
+
+
+
+test('update user password',()=>{
+    expect(UserTest.update("1",undefined,"11",undefined,undefined)).toBeTruthy();
+})
+
+
+
+
+
+test('update user email',()=>{
+    expect(UserTest.update("1",undefined,undefined,"b@b.com",undefined)).toBeTruthy();
+})
+
+test('update user email with same email as other user',()=>{
+    expect(UserTest.update("1",undefined,undefined,"b@b.com",undefined)).toBeFalsy();
+})
+
+test('update user country',()=>{
+    expect(UserTest.update("1",undefined,undefined,undefined,"eg")).toBeTruthy();
+})
+
+test('get followers of user with no follow',()=>{
+    expect(UserTest.getUserFollowingArtist('4')).toBeFalsy();
+})
+
+test('get followings of user with folowings which aome of are not users',()=>{
+    expect(UserTest.getUserFollowingArtist('5')).toBeTruthy();
+})
+
+
+test('check email of not exisiting user',()=>{
+    expect(UserTest.checkmail('84')).toBeFalsy();
+})
+
+test('check if user has access to playlist private actions in playlist he doesnt own and with zero playlist of his own',()=>{
+    expect(UserTest.checkAuthorizedPlaylist('4','5')).toBeFalsy();
+})
+
+test('user has zero playlist creates a playlist',()=>{
+    expect(UserTest.createdPlaylist('4','any playlist name','info')).toEqual([{
+        playListId: 'any playlist name',
+        addedAt: '20-03-2020', 
+        isPrivate: false
+    }]);
+})
+
+test('user has zero playlist check auth for edit on playlists',()=>{
+    expect(UserTest.checkAuthorizedPlaylist('5','1')).toBeFalsy();
+})
+
+test('user doesnt own playlist check for auth',()=>{
+    expect(UserTest.checkAuthorizedPlaylist('6','6')).toBeFalsy();
+})
+
+
+test('promote non user to artist',()=>{
+    expect(UserTest.promoteToArtist('84')).toBeFalsy();
+})
+
+
+
+// delete account at end 
 test('delete account ', () => {
     UserTest.deleteAccount('1');
     expect(UserTest.getUserById('1')).toEqual(undefined);
+})
+
+    test('delete non existing account ', () => {
+    
+    expect(UserTest.deleteAccount('99')).toBeFalsy();
 })
